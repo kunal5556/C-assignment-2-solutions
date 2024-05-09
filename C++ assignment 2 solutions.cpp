46701f5dbe5093023f04e018c4967f1630ad91a0
kunal5556 <169072606+kunal5556@users.noreply.github.com>
2024-05-09 16:25:35 +0530

    C++ assignment 2 solutions

---
+++ C++ assignment 2 solutions.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*
+    
+    question 1
+    output
+    Enter first number
+    2
+    Enter second number and value for taking modulus
+    3
+    6
+    Output is: 0
+
+    question 2
+    output
+    Enter first number
+    3
+    Enter second number
+    2
+    1 1
+
+    question 3
+    output
+    2
+    3
+    2
+
+    */
+
+    //question 5
+    //solution
+    char ch1 , ch2 ;
+    cout<<"Enter two characters : ";
+    cin>>ch1>>ch2;
+    cout<<"Difference between ASCII of "<<ch1<<" and "<<ch2<<" is : "<<(int)ch1 - (int)ch2;
+
+    /*
+    
+    question 6
+    output
+    49
+    
+    */
+
+}
